a43afeed4bdbf84d6469aa9e6fc30122c102567a
gaozhang.vc <gaozhang.vc@bytedance.com>
2022-11-07 21:05:22 +0800

    update pages

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/css/page.css","a61d510eb4aee37c0f17d59c8741fc70"],["/css/style.min.css","16a4cc20ac2ecce2dfd8bc11dcf43592"],["/css/vue-pdf.css","53d1349d48094a9bd73b6d74783970b5"],["/en/index.html","156b02df430b7092ad5f3adf33a903fb"],["/index.html","278994f05b7b4544ff25979178d8c8b5"],["/js/common.js","ae798d30af603c3cbf2fa6549151dd0b"],["/js/css.escape.js","fe4db48c9e3f272a6d12cf1312de889e"],["/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"]];
+var precacheConfig = [["/css/page.css","8eb13813d57cb95760fe2dd9497e668f"],["/css/style.min.css","16a4cc20ac2ecce2dfd8bc11dcf43592"],["/css/vue-pdf.css","57d4b3bb835f934f1f68584c77524e7b"],["/en/index.html","ad150ceeb7b1fc58ecce5b4964ded785"],["/index.html","c8cee9b001773a7a4438fa5903f50216"],["/js/common.js","ae798d30af603c3cbf2fa6549151dd0b"],["/js/css.escape.js","fe4db48c9e3f272a6d12cf1312de889e"],["/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
